ab5ac42b0e18eeb70f376c6a8fa91b93a7399f64
SobolevYuri <yura.sobolev.91@gmail.com>
2018-03-01 23:40:13 +0300

    My 10-th task, VERY LONG arrays =)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+  if(array.length==0) return 0;
+  array.sort(function(a,b) {return (a-b)});
+let len=0, SeqCnt=[];
+
+for(let i=1;i<array.length;i++){
+  if(array[i]-array[i-1] == 1)
+{
+  SeqCnt.push(array[i]);
+}else if(array[i] != array[i-1]){
+  SeqCnt=[];
 }
+len=Math.max(SeqCnt.length,len);
+}
+
+return ++len;
+}
+
